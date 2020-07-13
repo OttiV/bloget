4cd193820db9ef7420b2d829003bf61909df5e0d
Vgnttv <o.vignani@gmail.com>
2020-07-13 15:59:16 +0200

    Set up and basic implementation of components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import { Post, SearchBox } from './components';
 import './App.css';
 
 function App() {
+  const [search, setSearch] = useState('');
+  const [post, setPost] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setSearch('');
+  }, []);
+  const handleInput = event => {
+    setSearch(event.target.value);
+  };
+  const searchPost = () => {
+    fetch(
+      `https://t6uyyfsdg7.execute-api.eu-central-1.amazonaws.com/dev/get-url?url=https://medium.com/eli5/a-modern-setup-for-digital-products-627e8c25efca%60&url=${search}?format=jason`,
+      {
+        headers: {
+          'x-api-key': 'Vmlg5egOs04xE0KeiecGa2dKpEZek4KZ1kHpm0pm'
+        }
+      }
+    )
+      .then(response => response.json())
+      .then(res => {
+        setPost(res.data);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  };
+  console.log('post', post);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SearchBox
+        search={search}
+        handleInput={handleInput}
+        searchPost={searchPost}
+      />
+      <Post post={post} loading={loading} />
     </div>
   );
 }
